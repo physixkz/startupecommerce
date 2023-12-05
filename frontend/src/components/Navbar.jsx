7aba2afc0f8c0fb0a4aa0e0ccdd952e1b4f98180
physixkz <130267624+physixkz@users.noreply.github.com>
2023-12-05 00:16:04 -0800

    added test pages

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
+  return (
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </div>
+      <div className="links">
+        <Link to="/"> Home </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
