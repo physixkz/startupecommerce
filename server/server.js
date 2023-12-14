e202dd398124000de110eaab1f3dbebdad805d17
physixkz <130267624+physixkz@users.noreply.github.com>
2023-12-13 17:04:55 -0800

    update

--- server/server.js
+++ server/server.js
@@ -3,13 +3,16 @@ const path = require('path');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const mongoose = require('mongoose');
 const productRoutes = require('./routes/api/product-routes'); // Adjust the path accordingly
 const userRoutes = require('./routes/api/user-routes');
 const cors = require('cors');
 
+require('dotenv').config(); // Load environment variables from .env file
 
 const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/quantum_db';
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -22,11 +25,20 @@ app.use(cors()); // Enable CORS for all routes
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/hello', (req, res) => {
-  res.send('Hello, this is a custom route!');
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB successfully!');
+});
 
 // Mount the product routes
 app.use('/api/products', productRoutes);
@@ -36,10 +48,8 @@ app.use('/api/users', userRoutes); // Mount user routes
 server.start().then(() => {
   app.use('/graphql', expressMiddleware(server));
 
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    });
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
   });
 });
